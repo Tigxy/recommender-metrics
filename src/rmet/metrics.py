dbea3fb7f73d5d0a353a6ccca9079ac2cbd038e3
tigxy <54410868+Tigxy@users.noreply.github.com>
2024-03-08 09:27:15 +0100

    Merge _calculate() and calculate() functions and refactor code

--- src/rmet/metrics.py
+++ src/rmet/metrics.py
@@ -160,11 +160,11 @@ def coverage(logits: torch.Tensor, k: int = 10):
     """
     top_indices = _get_top_k(logits, k, logits_are_top_indices=False, sorted=False)
     n_items = logits.shape[-1]
-    return coverage_from_top_k(top_indices, n_items)
+    return coverage_from_top_k(top_indices, k, n_items)
 
 
-def coverage_from_top_k(top_indices, n_items):
-    n_unique_recommended_items = top_indices.unique(sorted=False).shape[0]
+def coverage_from_top_k(top_indices: torch.Tensor, k: int, n_items: int):
+    n_unique_recommended_items = top_indices[:, :k].unique(sorted=False).shape[0]
     return n_unique_recommended_items / n_items
 
 
@@ -178,7 +178,7 @@ _metric_fn_map_user = {
 }
 
 _metric_fn_map_distribution = {
-    MetricEnum.Coverage: coverage
+    MetricEnum.Coverage: coverage_from_top_k
 }
 
 # List of metrics that are currently supported
@@ -187,62 +187,10 @@ supported_user_metrics = tuple(_metric_fn_map_user.keys())
 supported_distribution_metrics = tuple(_metric_fn_map_distribution.keys())
 
 
-def _calculate(metrics: Iterable[str | MetricEnum], logits: torch.Tensor, targets: torch.Tensor = None, k: int = 10,
-               best_logit_indices: torch.Tensor = None, return_aggregated: bool = True, return_individual: bool = False,
-               ):
-    """
-    Computes the values for a given list of metrics.
-
-    :param metrics: The list of metrics to compute. Check out 'supported_metrics' for a list of names.
-    :param logits: prediction matrix about item relevance
-    :param targets: 0/1 matrix encoding true item relevance, same shape as logits
-    :param k: top k items to consider
-    :param best_logit_indices: Sorted indices of best logits, this can be used to speed up computations
-    :param return_aggregated: Whether aggregated metric results should be returned.
-    :param return_individual: Whether the results for individual users should be returned
-    :return: a dictionary containing ...
-        {metric_name: value} if 'return_aggregated=True', and/or
-        {<metric_name>_individual: list_of_values} if return_individual=True'
-    """
-    if logits.shape[-1] < k:
-        raise ValueError(f"'k' must not be greater than the number of items ({k} > {logits.shape[-1]})!")
-
-    if not (return_individual or return_aggregated):
-        raise ValueError(f"Specify either 'return_individual' or 'return_aggregated' to receive results.")
-
-    raw_results = {}
-    for metric in metrics:
-        if metric in _metric_fn_map_distribution:
-            raw_results[str(metric)] = _metric_fn_map_distribution[metric](logits, k)
-
-        elif metric in _metric_fn_map_user:
-            if targets is None:
-                raise ValueError(f"'targets' is required to calculate '{metric}'!")
-            # use pre-computed best logit indices to speed up computations
-            if best_logit_indices is not None:
-                raw_results[str(metric)] = _metric_fn_map_user[metric](best_logit_indices, targets, k,
-                                                                       logits_are_top_indices=True)
-            else:
-                raw_results[str(metric)] = _metric_fn_map_user[metric](logits, targets, k,
-                                                                       logits_are_top_indices=False)
-
-        else:
-            raise ValueError(f"Metric '{metric}' not supported.")
-
-    results = {}
-    if return_aggregated:
-        results.update({k: torch.mean(v).item() if isinstance(v, torch.Tensor) else v
-                        for k, v in raw_results.items()})
-
-    if return_individual:
-        results.update({k + "_individual": v for k, v in raw_results.items()})
-
-    return results
-
-
-def calculate(metrics: Iterable[str | MetricEnum], logits: torch.Tensor, targets: torch.Tensor = None,
+def calculate(metrics: Iterable[str | MetricEnum], logits: torch.Tensor = None, targets: torch.Tensor = None,
               k: int | Iterable[int] = 10, return_aggregated: bool = True, return_individual: bool = False,
-              flatten_results: bool = False, flattened_parts_separator: str = "/", flattened_results_prefix: str = ""):
+              flatten_results: bool = False, flattened_parts_separator: str = "/", flattened_results_prefix: str = "",
+              n_items: int = None, best_logit_indices: torch.Tensor = None, return_best_logit_indices: bool = False):
     """
     Computes the values for a given list of metrics.
 
@@ -256,25 +204,87 @@ def calculate(metrics: Iterable[str | MetricEnum], logits: torch.Tensor, targets
                             Key is of format "{prefix}/{metric}@{k}" for separator "/"
     :param flattened_parts_separator: How to separate the individual parts of the flattened key
     :param flattened_results_prefix: Prefix to prepend to the flattened results key.
+    :param n_items: Number of items in dataset (in case only best logit indices are supplied)
+    :param best_logit_indices: Previously computed indices of the best logits in sorted order
+    :param return_best_logit_indices: Whether to return the indices of the best logits
     :return: a dictionary containing ...
         {metric_name: value} if 'return_aggregated=True', and/or
         {<metric_name>_individual: list_of_values} if 'return_individual=True'
     """
-    if logits.shape != targets.shape:
+
+    k = (k,) if isinstance(k, int) else k
+    max_k = max(k)
+
+    # ensure validity of supplied parameters
+    if logits is not None and logits.shape[-1] < max_k:
+        raise ValueError(f"'k' must not be greater than the number of logits "
+                         f"({max_k} > {logits.shape[-1]})!")
+
+    if best_logit_indices is not None and best_logit_indices.shape[-1] < max_k:
+        raise ValueError(f"'k' must not be greater than the number of best indices "
+                         f"({max_k} > {best_logit_indices.shape[-1]})!")
+
+    if logits is None and (best_logit_indices is None or n_items is None):
+        raise ValueError("Either logits or best_logit_indices+n_items must be supplied")
+
+    if best_logit_indices is None and logits.shape != targets.shape:
         raise ValueError(f"Logits and targets must be of same shape ({logits.shape} != {targets.shape})")
+
+    if not (return_individual or return_aggregated):
+        raise ValueError(f"Specify either 'return_individual' or 'return_aggregated' to receive results.")
+
+    n_items = n_items or logits.shape
+    # to speed up computations, only retrieve highest logit indices once (if not already supplied)
+    if best_logit_indices is None:
+        best_logit_indices = logits.topk(max_k, dim=-1, sorted=True).indices
 
     full_prefix = f"{flattened_results_prefix}{flattened_parts_separator}" if flattened_results_prefix else ""
 
-    k = (k,) if isinstance(k, int) else k
-    best_logit_indices = logits.topk(max(k), dim=-1, sorted=True).indices
-
     metric_results = dict() if flatten_results else defaultdict(lambda: dict())
+    # iterate over all k's and compute the metrics for them
     for ki in k:
-        for metric, v in _calculate(metrics, logits, targets, ki, best_logit_indices=best_logit_indices,
-                                    return_aggregated=return_aggregated, return_individual=return_individual).items():
+        raw_results = _compute_raw_results(metrics, ki, best_logit_indices, n_items, targets)
+
+        results = {}
+        if return_individual:
+            individual_results = {k + "_individual": v for k, v in raw_results.items() if isinstance(v, torch.Tensor)}
+            results.update(individual_results)
+
+        if return_aggregated:
+            aggregated_results = _aggregate_results(raw_results)
+            results.update(aggregated_results)
+
+        for metric, v in results.items():
             if flatten_results:
                 metric_results[f"{full_prefix}{metric}@{ki}"] = v
             else:
                 metric_results[metric][ki] = v
 
+    if return_best_logit_indices:
+        return dict(metric_results), best_logit_indices
+
     return dict(metric_results)
+
+
+def _compute_raw_results(metrics: Iterable[str | MetricEnum], k: int,
+                         best_logit_indices: torch.Tensor, n_items: int, targets: torch.Tensor = None):
+    raw_results = {}
+    for metric in metrics:
+        if metric in _metric_fn_map_distribution:
+            raw_results[str(metric)] = _metric_fn_map_distribution[metric](best_logit_indices, k, n_items)
+
+        elif metric in _metric_fn_map_user:
+            if targets is None:
+                raise ValueError(f"'targets' is required to calculate '{metric}'!")
+            # use pre-computed best logit indices to speed up computations
+            raw_results[str(metric)] = _metric_fn_map_user[metric](best_logit_indices, targets, k,
+                                                                   logits_are_top_indices=True)
+
+        else:
+            raise ValueError(f"Metric '{metric}' not supported.")
+    return raw_results
+
+
+def _aggregate_results(raw_results):
+    return {k: torch.mean(v).item() if isinstance(v, torch.Tensor) else v
+            for k, v in raw_results.items()}
